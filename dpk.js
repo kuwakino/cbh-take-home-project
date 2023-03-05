8dc664a56b0256b2530660f270055ec3245a2989
Eduardo Kuwakino <eduardo.kuwakino@eyko.io>
2023-03-05 11:49:39 -0300

    feat(dpk): reafactorings;

--- dpk.js
+++ dpk.js
@@ -3,26 +3,31 @@ const crypto = require("crypto");
 exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
+  let keyCandidate;
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
-  }
+if (!event) return TRIVIAL_PARTITION_KEY;
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
+  if (event.partitionKey) {
+    keyCandidate = event.partitionKey;
   } else {
-    candidate = TRIVIAL_PARTITION_KEY;
+    const data = JSON.stringify(event);
+    keyCandidate = hashKey(data);
   }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
-  }
-  return candidate;
+  
+  return  validatePartitionKey(keyCandidate, MAX_PARTITION_KEY_LENGTH);
 };
+
+function validatePartitionKey(keyCandidate, MAX_PARTITION_KEY_LENGTH) {
+  if (typeof keyCandidate !== "string") {
+    keyCandidate = JSON.stringify(keyCandidate);
+  }
+
+  if (keyCandidate.length > MAX_PARTITION_KEY_LENGTH) {
+    keyCandidate = hashKey(keyCandidate);
+  }
+  return keyCandidate;
+}
+
+function hashKey(keyCandidate) {
+  return crypto.createHash("sha3-512").update(keyCandidate).digest("hex");
+}
